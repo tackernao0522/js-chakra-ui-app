39fd6e608cf56097ca1e4fd7b7e1d8a5afab6d45
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2021-11-22 16:42:30 +0900

    ログインユーザー情報をContextに保持してみる

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,7 +1,8 @@
-/* eslint-disabled react-hooks/exhaustive-deps */
+/* eslint-disable react-hooks/exhaustive-deps */
 import { Center, Wrap, Spinner, WrapItem, useDisclosure } from "@chakra-ui/react";
 import { memo, useCallback, useEffect, VFC } from "react";
 import { useAllUsers } from "../../hooks/useAllUsers";
+import { useLoginUser } from "../../hooks/useLoginUser";
 import { useSelectUser } from "../../hooks/useSelectUser";
 import { UserCard } from "../organisms/user/UserCard";
 import { UserDetailModal } from "../organisms/user/UserDetailModal";
@@ -10,6 +11,8 @@ export const UserManagement: VFC = memo(() => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const { getUsers, users, loading } = useAllUsers();
   const { onSelectUser, selectedUser } = useSelectUser();
+  const { loginUser } = useLoginUser();
+  console.log(loginUser);
 
   useEffect(() => getUsers(), [])
 
